8c5007786c6ecb0c0904b581463993075af1e128
Hazdryx <hazdryx@gmail.com>
2020-09-17 16:38:04 -0500

    Removed unused usings.

--- FastBitmapExt/ColorF.cs
+++ FastBitmapExt/ColorF.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.IO.IsolatedStorage;
-using System.Net;
-using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace Hazdryx.Drawing.Extension
 {
