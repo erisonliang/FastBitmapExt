46a24edfbdf7c80b8990767d2109621d3ede767e
Hazdryx <hazdryx@gmail.com>
2020-09-17 06:01:42 -0500

    Add project files.

---
+++ FastBitmapExt/FastBitmapExt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hazdryx.Drawing
+{
+    /// <summary>
+    ///     A class which contains various extensions for FastBitmap.
+    /// </summary>
+    public static class FastBitmapExt
+    {
+    }
+}
